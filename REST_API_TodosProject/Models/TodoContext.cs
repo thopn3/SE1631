526ef02e02375d3d34476bfbb067681827e97290
thopn3 <thopn3@fpt.edu.vn>
2024-01-10 14:23:49 +0700

    TodoItems API sample

---
+++ REST_API_TodosProject/Models/TodoContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace REST_API_TodosProject.Models
+{
+    public class TodoContext: DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
